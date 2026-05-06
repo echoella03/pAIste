b876870aced1ae364181969703cbb1dc11f4fea4
Daniela Comapon <128163882+echoella03@users.noreply.github.com>
2026-05-07 07:13:01 +0800

    Update main.py

--- pAIste-backend/app/main.py
+++ pAIste-backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import create_db_and_tables
 from app.api.routes import auth, reports, identify, admin, dashboard, map, species
 from fastapi.staticfiles import StaticFiles
-
+import os
 
 app = FastAPI(
     title="pAIste API",
@@ -11,11 +11,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+if os.path.exists("uploads"):
+    app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+origins = [
+    "https://p-a-iste-gqpxiacmt-echoella03s-projects.vercel.app", 
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # update with your frontend URL in production
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,8 +37,9 @@ app.include_router(reports.router,   prefix="/reports",   tags=["Reports"])
 app.include_router(admin.router,     prefix="/admin",     tags=["Admin"])
 app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 app.include_router(map.router,       prefix="/map",       tags=["Map"])
-app.include_router(species.router, prefix="/species", tags=["Species"])
+app.include_router(species.router,   prefix="/species",   tags=["Species"])
 
 @app.get("/")
 def root():
     return {"message": "Welcome to pAIste API"}
+app = app
